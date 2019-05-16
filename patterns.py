248da7bcb59fe84e9fe2b957e82bba3041cba7ce
Jarrett Knauer <jarrettknauer@gmail.com>
2019-05-16 12:38:10 -0600

    added implementation outline

--- patterns.py
+++ patterns.py
@@ -1,1 +1,25 @@
 # TODO: 3x3 pattern implementation
+# Outline
+#
+# THE GAME - pattern representations
+# 1. Name
+# 2. Position
+# 3. Contract
+# 4. Strategy
+# 
+# THE PLAYER - interface
+# 1. initialize the board
+# G <--- Empty 3x3 board
+# Stack <--- { G }
+# 
+# 2. black first move (computer)
+# pop G from stack
+# add play(G,x) to the stack, where x is any given cell (we will place on the main diagonal)
+# while stack is not empty do:
+#       get opponent's move x
+#       if pattern exists in G in S:
+#           pop G from stack
+#           add play(G,x) to stack
+#       else:
+#           pop any G from stack
+#           add play(G,0)
