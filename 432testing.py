8c7cda66109ff96c7436ad11fea38a0eef3a98db
Scott-Dupasquier <sdupasqu@ualberta.ca>
2019-06-19 11:44:02 -0600

    Finished replying moves in a 432 pattern

--- 432testing.py
+++ 432testing.py
@@ -1,15 +1,15 @@
 def main():
     pattern = ['d2', 'c3', 'b3', 'd3', 'a4', 'b4', 'c4', 'd4'] # Bottom Right
-    reply432(change_pattern(pattern), pos_2_coord('c2'), pos_2_coord('d3'))
+    print(reply432(change_pattern(pattern), pos_2_coord('c2'), pos_2_coord('b3')))
     
     pattern = ['a3', 'a2', 'b2', 'c2', 'a1', 'b1', 'c1', 'd1'] # Top Left
-    reply432(change_pattern(pattern), pos_2_coord('b3'), pos_2_coord('b2'))
+    print(reply432(change_pattern(pattern), pos_2_coord('b3'), pos_2_coord('c2')))
     
     pattern = ['c2', 'b3', 'c3', 'd3', 'a4', 'b4', 'c4', 'd4'] # Bottom Left
-    reply432(change_pattern(pattern), pos_2_coord('d2'), pos_2_coord('b3'))
+    print(reply432(change_pattern(pattern), pos_2_coord('d2'), pos_2_coord('d3')))
     
     pattern = ['b3', 'a2', 'b2', 'c2', 'a1', 'b1', 'c1', 'd1'] # Top Right
-    reply432(change_pattern(pattern), pos_2_coord('a3'), pos_2_coord('c2'))
+    print(reply432(change_pattern(pattern), pos_2_coord('a3'), pos_2_coord('a2')))
 
 def pos_2_coord(pos):
     # Changes a board coordinate into normal coordinates
@@ -28,11 +28,24 @@ def reply432(pattern, black_pos, white_move):
             max_x_dist = dist
             max_x_pos = coord
             
-    # Find the corner part of the triangle for the 432 pattern
+    # Find the corner part of the triangle for the 432 pattern and the connecting
+    # cell for the other 5 cells
     if abs(max_x_dist) >= 3:
         triangle_pos = (max_x_pos[0] - max_x_dist, max_x_pos[1])
+        
+        # Find connection cell for 5 pattern
+        if (max_x_pos[0] + 1, max_x_pos[1] - 1) in pattern:
+            five_pos = (max_x_pos[0] + 1, max_x_pos[1] - 1)
+        else:
+            five_pos = (max_x_pos[0] - 1, max_x_pos[1] + 1)
     else:
         triangle_pos = max_x_pos
+        
+        # Find connecting cell for 5 pattern
+        if (max_x_pos[0] - 3, max_x_pos[1] + 1) in pattern:
+            five_pos = (max_x_pos[0] - 3, max_x_pos[1] + 1)
+        else:
+            five_pos = (max_x_pos[0] + 3, max_x_pos[1] - 1)
         
     # Find the last two cells to complete the triangle
     triangle = [triangle_pos] + list(set(pattern) & set(find_neighbours(triangle_pos[0], triangle_pos[1])))
@@ -40,10 +53,25 @@ def reply432(pattern, black_pos, white_move):
     
     if white_move in triangle:
         # Need to perform replying move in other 5 cells
-        pass
+        #five_pattern = []
+        #for cell in pattern:
+            #if cell not in triangle:
+                #five_pattern.append(cell)
+        return five_pos
+        
     else:
         # Need to perform replying move in triangle
-        pass
+        y_coords = ''
+        for coord in triangle:
+            y_coords += str(coord[1])
+        
+        for num in y_coords:
+            count = 0
+            for item in y_coords:
+                if item == num:
+                    count += 1
+            if count == 1:
+                return triangle[y_coords.index(num)]
     return
 
 def change_pattern(pattern):
